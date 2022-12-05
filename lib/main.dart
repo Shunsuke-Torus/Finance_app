ac0f1b0d10f8aae4aca6c9002fcf6a91534f1a00
Shun <shunsuke.takasu.job@gmail.com>
2022-12-05 23:57:14 +0900

    Add:追加ボタンを押すことでシートを表示するようにした｡

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'widgets/user_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,35 +17,103 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class _MyHomePage extends StatelessWidget {
+class _MyHomePage extends StatefulWidget {
+  @override
+  State<_MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Bread",
+      amount: 8.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t3",
+      title: "New PC",
+      amount: 1499.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+    //更新するsetState
+    setState(() {
+      _userTransaction.add(newTx);
+      // スプレッド演算子で展開せずにポインタを受け継いだ状態で_userTransaction内に入れる｡
+      // _userTransaction = [..._userTransaction, newTx];
+    });
+  }
+
+  void _startAddNewtransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(addTx: _addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Finance App"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                child: Card(
-                  color: Colors.blue,
-                  elevation: 5,
-                  child: Container(
-                    width: double.infinity,
-                    child: Text("Cfhart!"),
-                  ),
+      appBar: AppBar(
+        title: Text("Finance App"),
+        actions: [
+          IconButton(
+            onPressed: () => _startAddNewtransaction(context),
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Container(
+                  width: double.infinity,
+                  child: Text("Chart!"),
                 ),
               ),
-              UserTransactions(),
-              Card(
-                color: Colors.red,
-                child: Text("List Of Tx"),
-              ),
-            ],
-          ),
-        ));
+            ),
+            TransactionList(transactions: _userTransaction),
+            Card(
+              color: Colors.red,
+              child: Text("List Of Tx"),
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _startAddNewtransaction(context),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
